a0ffd682a0fa1fea59870393def50242a2ca4b2c
sulemigoreng <sulemigoreng@gmail.com>
2024-11-14 17:45:09 +0700

    Feat : add detail page
    - Add detail page
    - Add action on list pokemon

--- app/components/moleculs/CardPokemon/index.tsx
+++ app/components/moleculs/CardPokemon/index.tsx
@@ -1,23 +1,26 @@
 import { PokemonData } from "@/app/interface/interface"
 import { TypeBadge } from "../../atoms"
+import Link from "next/link"
 
 export default function CardPokemon({ item }: { item: PokemonData }) {
     return (
         <>
             <div className="p-4 bg-zinc-100 rounded-xl flex flex-col gap-2 xl:w-[12%] sm:w-[50%] items-center cursor-pointer">
-                <div><img src={item.sprites.front_default} alt="Pokemon Sprites" className="w-full"/></div>
-                <div className="text-2xl font-bold font-pixel">
-                    {item.name.replace(/^[a-z]/, function (m: string) { return m.toUpperCase() })}
-                </div>
-                <div className="flex gap-2">
-                    {
-                        item.types.map(type => {
-                            return (
-                                <TypeBadge type={type.type.name} key={type.slot}/>
-                            )
-                        })
-                    }
-                </div>
+                <Link href={`/detail/${item.id}`}>
+                    <div><img src={item.sprites.front_default} alt="Pokemon Sprites" className="w-full" /></div>
+                    <div className="text-2xl font-bold font-pixel">
+                        {item.name.replace(/^[a-z]/, function (m: string) { return m.toUpperCase() })}
+                    </div>
+                    <div className="flex gap-2">
+                        {
+                            item.types.map(type => {
+                                return (
+                                    <TypeBadge type={type.type.name} key={type.slot} />
+                                )
+                            })
+                        }
+                    </div>
+                </Link>
             </div>
         </>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
-import Image from "next/image";
 import { FilterType, LoadMore } from "./components/organism";
-import { FilterProvider } from "./context/FilterContext";
 export default async function Home() {
   return (
-    <FilterProvider>
-      <div className={`flex flex-col gap-4 text-white h-full p-8`}>
-        <div className="flex items-center gap-4">
-          <Image src="pokemon.svg" alt="pokemonLogo" width={30} height={100} />
-          <h1 className="text-2xl">Pokédex</h1>
-        </div>
-        {/* <div>
-          <input type="text" name="search" id="search" placeholder="Search"
-            className="p-2 rounded-xl w-full text-black pl-4" />
-        </div> */}
-        <FilterType />
-        <LoadMore />
-      </div>
-    </FilterProvider>
+    <>
+      <FilterType />
+      <LoadMore />
+    </>
   );
 }
